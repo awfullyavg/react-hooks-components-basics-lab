2496138a2d3c9f4cb0b15b2d448ef98fb2a803c7
Bryant Wiggins <topwigit@gmail.com>
2023-05-22 11:26:01 -0400

    another lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About () {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
